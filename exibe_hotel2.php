1e3fd9244db2ab35d3c2d4c9d02ef5598d49564e
ronaltjunior <70353643+ronaltjunior@users.noreply.github.com>
2020-10-08 17:54:21 -0300

    Update exibe_hotel2.php

--- exibe_hotel2.php
+++ exibe_hotel2.php
@@ -11,3 +11,25 @@
 
     </body>
 </html>
+
+
+resolução do trabalho
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Resultado da reserva</title>
+    </head>
+    <body>
+<script>
+document.writeln(name);
+document.writeln("seu cadastro foi realizado com sucesso");
+document.writeln("sua data de chegada é" + data_chegada.d);
+
+<script>
+
+<p><strong>ronalt junior</strong> seu cadastro foi realizado com sucesso</p><p>Sua data de chegada é: 08/10/20<strong>20</strong> e você ficará <strong>2</strong> noites e p
+
+    </body>
+</html>
